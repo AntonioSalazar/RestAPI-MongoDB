9667e3f867b6a83fce35b582afa1d36e0a7949fa
Salazar <antonio.salazar@oliverwyman.com>
2019-05-02 14:34:18 -0500

    first commit REST Project using Mongo

---
+++ controllers/departmentController.js
@@ -0,0 +1,28 @@
+
+function listAllDepartments (req, res) {
+
+}
+
+function listOneDepartment(req, res) {
+
+}
+
+function createDepartment(req, res) {
+
+}
+
+function updateDepartment(req, res) {
+
+}
+
+function deleteDepartment(req, res) {
+
+}
+
+module.exports = {
+    listAllDepartments,
+    listOneDepartment,
+    createDepartment,
+    updateDepartment,
+    deleteDepartment
+}
---
+++ controllers/employeesController.js
@@ -0,0 +1,28 @@
+
+function listAllEmployees (req, res) {
+
+}
+
+function listOneEmployee(req, res) {
+
+}
+
+function createEmployee(req, res) {
+
+}
+
+function updateEmployee(req, res) {
+
+}
+
+function deleteEmployee(req, res) {
+
+}
+
+module.exports = {
+    listAllEmployees,
+    listOneEmployee,
+    createEmployee,
+    updateEmployee,
+    deleteEmployee
+}
