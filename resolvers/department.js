cc1985c597283f1a5d0678b38cccc6bc13106d21
Salazar <antonio.salazar@oliverwyman.com>
2019-05-09 08:43:44 -0500

    added couple graphql endpoints

---
+++ resolvers/department.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const base = `http://localhost:3000`;
+
+module.exports = {
+    Query: {
+        department: async(parent, {deptName}) => {
+            return await fetch(`${base}/departments/${deptName}/employees`).then(response => response.json());
+        }
+    },
+    Department: {
+        employees: async parent => {
+            console.log(parent);
+            for (let i = 0; i < parent.length; i++) {
+                let deptName = parent[i].department.name;
+                return await fetch(`${base}/departments/${deptName}/employees`).then(response => response.json());
+            }   
+        }
+    }
+}
---
+++ resolvers/employee.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+const base = `http://localhost:3000`;
+
+module.exports = {
+    Query: {
+        employees : async ( ) => {
+            return await fetch(`${base}/employees`).then(response => response.json());
+        },
+        employee : async (parent, {_id}, context) => {
+            return await fetch(`${base}/employees/${_id}`, {
+                method: 'GET',
+                headers: {
+                    Authorization: context.authHeader
+                }
+            }).then(response => response.json())
+        }
+    }
+};
